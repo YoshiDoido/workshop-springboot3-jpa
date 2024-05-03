b9d36a7e6f2fce8d183fc2d687710bffda9da320
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-03 17:49:29 -0300

    Exception handling - update

--- src/main/java/com/fatecdiadema/course/services/UserService.java
+++ src/main/java/com/fatecdiadema/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.fatecdiadema.course.entities.User;
 import com.fatecdiadema.course.repositories.UserRepository;
 import com.fatecdiadema.course.services.exceptions.DatabaseException;
 import com.fatecdiadema.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
