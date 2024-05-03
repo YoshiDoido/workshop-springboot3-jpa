8e0efbbeaa062e066cb99aac36de2b373f1eb8ae
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-03 17:46:36 -0300

    Exception handling - delete

--- src/main/java/com/fatecdiadema/course/services/UserService.java
+++ src/main/java/com/fatecdiadema/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.fatecdiadema.course.services;
 
 import com.fatecdiadema.course.entities.User;
 import com.fatecdiadema.course.repositories.UserRepository;
+import com.fatecdiadema.course.services.exceptions.DatabaseException;
 import com.fatecdiadema.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
